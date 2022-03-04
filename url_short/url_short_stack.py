6f44a77d89f8de633692f8becaa545694c800ad0
BugKiller-tech <yaroslav.techlead@gmail.com>
2022-03-04 09:43:55 +0200

    complete cdk with python

--- url_short/url_short_stack.py
+++ url_short/url_short_stack.py
@@ -2,6 +2,9 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_dynamodb,
+    aws_lambda,
+    aws_apigateway
 )
 from constructs import Construct
 
@@ -17,3 +20,16 @@ class UrlShortStack(Stack):
         #     self, "UrlShortQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        table = aws_dynamodb.Table(self, "mapping", partition_key=aws_dynamodb.Attribute(name='id', type=aws_dynamodb.AttributeType.STRING))
+
+        function = aws_lambda.Function(self, "backend",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            handler="handler.main",
+            code=aws_lambda.Code.from_asset("./lambda"))
+        
+        table.grant_read_write_data(function)
+
+        function.add_environment("TABLE_NAME", table.table_name)
+
+        api = aws_apigateway.LambdaRestApi(self, "api", handler=function)
+
